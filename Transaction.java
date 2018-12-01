eb9e7b8030310bd02676f98c7a84dc091be11df3
swaahire <44641075+swaahire@users.noreply.github.com>
2018-12-01 14:04:51 +0530

    update

---
+++ Transaction.java
@@ -0,0 +1,28 @@
+package ExampleofStream;
+
+public class Transaction {
+
+	Trader trader; 
+	int year,value;
+	public Transaction(Trader trader, int year, int value) {
+		super();
+		this.trader = trader;
+		this.year = year;
+		this.value = value;
+	}
+	public Trader getTrader() {
+		return trader;
+	}
+	public int getYear() {
+		return year;
+	}
+	public int getValue() {
+		return value;
+	}
+	@Override
+	public String toString() {
+		return "Transaction [trader=" + trader + ", year=" + year + ", value="
+				+ value + "]";
+	} 
+	
+}
